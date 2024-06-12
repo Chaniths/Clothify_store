baa76f6e309e0262d6f7e0b60cb9a203f0c7b571
Chaniths <wijekoonchanith12@gmail.com>
2024-06-12 11:37:37 +0530

    Innitial Commit

---
+++ src/main/java/com/clothify/pos/controller/user/LoginFormController.java
@@ -0,0 +1,25 @@
+package com.clothify.pos.controller.user;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Hyperlink;
+
+public class LoginFormController {
+    public JFXTextField txtEmail;
+    public JFXButton btnLogin;
+    public Hyperlink linkForgotPassword;
+    public Hyperlink linkCreateAccount;
+    public JFXPasswordField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void linkForgotPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void linkCreateAccountOnAction(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/main/java/com/clothify/pos/controller/user/RegisterFormController.java
@@ -0,0 +1,23 @@
+package com.clothify.pos.controller.user;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Hyperlink;
+
+public class RegisterFormController {
+    public Hyperlink linkGoToLogin;
+    public JFXButton btnSignUp;
+    public JFXTextField txtFirstName;
+    public JFXTextField txtLastName;
+    public JFXTextField txtUserName;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+
+    public void linkGoToLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSignUpOnAction(ActionEvent actionEvent) {
+    }
+}
